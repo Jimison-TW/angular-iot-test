f131c1714c490c2600402559dcf6d53361dba3a1
fish <>
2025-10-20 01:24:57 +0800

    - 新增dashboard頁面 - 將dashbaord加入router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // 預設導向 Dashboard
+  { path: 'dashboard', component: Dashboard },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
