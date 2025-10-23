b895100add400e1eb1d051fb174e94f51bd095f1
fish <>
2025-10-23 11:28:52 +0800

    建立header、sidebar、main content的結構

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { DrawerModule } from 'primeng/drawer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule, ButtonModule, DrawerModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('test-app');
+  protected readonly title = signal('天氣監控系統');
+  sidebarVisible = signal(true);
+
+  cities = [
+    { name: '台北', route: '/weather/taipei' },
+    { name: '台中', route: '/weather/taichung' },
+    { name: '高雄', route: '/weather/kaohsiung' }
+  ];
 }
